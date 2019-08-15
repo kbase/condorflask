5acbafe8cb3e2d574fca77360c50783f26b51527
Matyas Selmeci <matyas@cs.wisc.edu>
2019-08-15 16:39:22 -0500

    Test jobs.
    Which are apparently broken.

--- tests/apid_test.py
+++ tests/apid_test.py
@@ -32,6 +32,10 @@ def checked_get(uri, params=None):
     return r
 
 
+def checked_get_json(uri, params=None):
+    return checked_get(uri, params=params).json()
+
+
 def test_condor_version(fixtures):
     r = checked_get("v1/config/condor_version")
     assert re.search(r"\d+\.\d+\.\d+", r.text), "Unexpected condor_version"
@@ -45,8 +49,37 @@ def test_status(fixtures):
             assert j[0].get(attr), "%s: %s attr missing" % (daemon, attr)
 
 
-def test_history(fixtures):
-    r = checked_get("v1/history")
-    j = r.json()
+def check_job_attrs(job):
     for attr in ["classad", "jobid"]:
-        assert j[0].get(attr), "%s attr missing" % (attr)
+        assert job.get(attr), "%s attr missing" % attr
+
+
+def test_history(fixtures):
+    j = checked_get_json("v1/history")
+    check_job_attrs(j[0])
+
+
+def submit_job():
+    """Submit a sleep job and return the cluster ID"""
+    sub = htcondor.Submit({
+        "Executable": "/usr/bin/sleep",
+        "Arguments": "300",
+    })
+    schedd = htcondor.Schedd()
+    with schedd.transaction() as txn:
+        cluster_id = sub.queue(txn)
+    return cluster_id
+
+
+def rm_cluster(cluster_id):
+    schedd = htcondor.Schedd()
+    schedd.act(htcondor.JobAction.Remove, "ClusterId == %d" % cluster_id)
+
+
+def test_jobs(fixtures):
+    cluster_id = submit_job()
+    queries = ["v1/jobs", "v1/jobs/%d" % cluster_id, "v1/jobs/%d/0" % cluster_id]
+    for q in queries:
+        j = checked_get_json(q)
+        check_job_attrs(j[0])
+    rm_cluster(cluster_id)
