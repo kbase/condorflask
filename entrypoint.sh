38e2a503ab0ea2e4024ffba28f7b2567d9353283
bio-boris <bio-boris@users.noreply.github.com>
2019-09-10 12:58:29 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
-  useradd kbase
-  if [ "${POOL_PASSWORD}" ] ; then
-        /usr/sbin/condor_store_cred -p "${POOL_PASSWORD}" -f /etc/condor/password
-  fi
-  chown kbase /etc/condor/password
-  
-  chmod +x rungunicorn && ./rungunicorn
+mkdir -p /etc/condor/
+if [ "${POOL_PASSWORD}" ] ; then
+  /usr/sbin/condor_store_cred -p "${POOL_PASSWORD}" -f /etc/condor/password
+fi
+chmod +x rungunicorn && ./rungunicorn
